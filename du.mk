8853e5b0d6d21112f0dadd28e7528ae7c4e53bc8
nobe1976 <charlesmc76@gmail.com>
2019-02-25 15:20:32 -0500

    Changes to build DU

--- du.mk
+++ du.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/lge/d850/full_d850.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_d850
+PRODUCT_NAME := du_d850
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo du_d850-user
+add_lunch_combo du_d850-userdebug
